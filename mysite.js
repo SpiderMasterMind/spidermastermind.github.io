f7a19bdf86cb5eb99060fbae844f7987dae59ddd
Ben Stacey <benstacey@MacBook-Pro.home>
2017-11-30 22:15:13 +0000

    anim speed

--- mysite.js
+++ mysite.js
@@ -2,11 +2,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var ben = document.getElementById("ben");
 	var stacey = document.getElementById("stacey");
 	var gridbox = document.getElementById("gridbox");
-// TweenLite.from(stacey, 0.7,  { y: -140 });
-// TweenLite.from(ben, 0.3,  { y: -99 });
-
-// draw grid
-
 
 	for (var i = 0; i < 240; i ++) {
 		var a = document.createElement("div");
@@ -14,16 +9,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		gridbox.appendChild(a)
 	}
 
-	// TweenLite.from(a, 1, { opacity: 0 });
-	TweenMax.staggerFrom(".cell", 1, {transform: "scale(0.01)"}, 0.02);
-
-
-
-
-
-
-
-
-
-
+	TweenMax.staggerFrom(".cell", 1, {transform: "scale(0.01)"}, 0.005);
 });
